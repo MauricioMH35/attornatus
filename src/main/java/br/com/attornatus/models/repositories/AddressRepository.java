32c8ea32f05d9e9901cebcc1779adca3c62cde25
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-02-18 02:35:32 -0300

    feat: address repository

---
+++ src/main/java/br/com/attornatus/models/repositories/AddressRepository.java
@@ -0,0 +1,29 @@
+package br.com.attornatus.models.repositories;
+
+import br.com.attornatus.models.entities.Address;
+import br.com.attornatus.models.enums.AddressRelevanceLevel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    @Query("SELECT a FROM Address a WHERE a.people.id = ?1")
+    Page<Address> findByPeopleId(@Param("peopleId") Long peopleId, Pageable pageable);
+
+    @Query("SELECT a FROM Address a WHERE a.people.id = ?1 AND a.relevanceLevel = 'HIGH'")
+    Address findHighRelevanceLevelByPeopleId(@Param("peopleId") Long peopleId);
+
+    Page<Address> findByRelevanceLevel(AddressRelevanceLevel relevanceLevel, Pageable pageable);
+
+    Page<Address> findByPublicPlaceContains(String plublicPlace, Pageable pageable);
+
+    Page<Address> findByCityContains(String city, Pageable pageable);
+
+    Page<Address> findByZipCode(String zipCode, Pageable pageable);
+
+}
