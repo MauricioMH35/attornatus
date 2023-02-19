8e4fcdb9e240609755799c9ccced872d5465bfc9
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-02-19 02:26:59 -0300

    feat: find by name contains page request test and implement

--- src/main/java/br/com/attornatus/resources/services/impls/PersonServiceFindByNameContainsImpl.java
+++ src/main/java/br/com/attornatus/resources/services/impls/PersonServiceFindByNameContainsImpl.java
@@ -1,5 +1,6 @@
 package br.com.attornatus.resources.services.impls;
 
+import br.com.attornatus.exceptions.NotFoundException;
 import br.com.attornatus.models.entities.Person;
 import br.com.attornatus.models.repositories.PersonRepository;
 import br.com.attornatus.resources.services.PersonServiceFindByNameContains;
@@ -7,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -20,7 +24,80 @@ public class PersonServiceFindByNameContainsImpl implements PersonServiceFindByN
 
     @Override
     public Page<Person> apply(String name, Map<String, String> pageParams) {
+        Pageable pageable = parsePageable(pageParams);
         return null;
+    }
+
+    private Pageable parsePageable(Map<String, String> pageParams) {
+        throwsExceptionWhenFieldsAreNotFound(pageParams);
+        throwsExceptionWhenFieldsAreBlank(pageParams);
+        throwsExceptionWhenPageFieldAreNotNumbers(pageParams);
+        throwsExceptionWhenFieldsAreLessThanZero(pageParams);
+
+        Integer page = Integer.parseInt(pageParams.get("page"));
+        Integer size = Integer.parseInt(pageParams.get("size"));
+
+        return PageRequest.of(page, size, Sort.by("name").ascending());
+    }
+
+    private boolean checkFieldIsNumeric(String field) {
+        try {
+            Integer.parseInt(field);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private void throwsExceptionWhenFieldsAreNotFound(Map<String, String> pageParams) {
+        boolean noExistsPageKey = !pageParams.containsKey("page");
+        boolean noExistsSizeKey = !pageParams.containsKey("size");
+
+        if (noExistsPageKey || noExistsSizeKey) {
+            log.error("Uma tentativa de consulta foi realizada para listar as pessoas cadastradas, mas não foram " +
+                    "informados os parêmtros `page` e `size` para realizar a operação.");
+
+            throw new IllegalArgumentException("Não é possivel realizar a operação, os parâmetros informados não são válidos.");
+        }
+    }
+
+    private void throwsExceptionWhenFieldsAreBlank(Map<String, String> pageParams) {
+        boolean isNullPageParam = pageParams.get("page") == "";
+        boolean isNullSizeParam = pageParams.get("size") == "";
+
+        if (isNullPageParam || isNullSizeParam) {
+            log.error("Uma tentativa de consulta foid realizada para listar as pessoas cadastradas, mas não foi " +
+                    "possivel realizar a operação devido ter sido informado os parêmtros `page` e `size` com valores " +
+                    "em branco.");
+
+            throw new IllegalArgumentException("Não é possivel realizar a operação, os parâmetros informados não são válidos.");
+        }
+    }
+
+    private void throwsExceptionWhenPageFieldAreNotNumbers(Map<String, String> pageParams) {
+        boolean isPageNotNumeric = !checkFieldIsNumeric(pageParams.get("page"));
+        boolean isSizeNotNumeric = !checkFieldIsNumeric(pageParams.get("size"));
+
+        if (isPageNotNumeric || isSizeNotNumeric) {
+            log.error("Uma tentativa de consulta foid realizada para listar as pessoas cadastradas, mas os parâmetros " +
+                    "informados `page` e `size` não são valores numéricos.");
+
+            throw new IllegalArgumentException("Não é possivel realizar a operação, os parâmetros `page` e `size` não " +
+                    "são valores numéricos.");
+        }
+    }
+
+    private void throwsExceptionWhenFieldsAreLessThanZero(Map<String, String> pageParams) {
+        boolean isPageLessThanZero = Integer.parseInt(pageParams.get("page")) < 0;
+        boolean isSizeLessThanZero = Integer.parseInt(pageParams.get("size")) < 0;
+
+        if (isPageLessThanZero || isSizeLessThanZero) {
+            log.error("Uma tentativa de consulta para organizar a paginação do resulta com valores menores do que zero " +
+                    "realizada.");
+            throw new IllegalArgumentException("Não é possivel realizar a operação com os parâmetros informados sendo " +
+                    "menores do que zero.");
+        }
     }
 
 }
--- src/test/java/br/com/attornatus/resources/services/impls/PersonServiceFindByNameContainsImplTest.java
+++ src/test/java/br/com/attornatus/resources/services/impls/PersonServiceFindByNameContainsImplTest.java
@@ -2,10 +2,17 @@ package br.com.attornatus.resources.services.impls;
 
 import br.com.attornatus.models.repositories.PersonRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 public class PersonServiceFindByNameContainsImplTest {
@@ -15,6 +22,78 @@ public class PersonServiceFindByNameContainsImplTest {
 
     @BeforeEach void setUp() {
         underTest = new PersonServiceFindByNameContainsImpl(repository);
+    }
+
+    @Test
+    @DisplayName("When Find By Name Contains Persons With Page Fields Not Found Should Throw An IllegalArgumentException")
+    void whenPageFieldsAreNotFound() {
+        final String name = "Flavia";
+        final Map<String, String> pageParams = Map.of("pageNoExist", "0", "sizeNoExist", "10");
+        final String exceptionMessage = "Não é possivel realizar a operação, os parâmetros informados não são válidos.";
+
+        assertThatThrownBy(() -> underTest.apply(name, pageParams))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(exceptionMessage);
+    }
+
+    @Test
+    @DisplayName("When Find By Name Contains Persons With Page Fields Are Blank Should Throw An IllegalArgumentException")
+    void whenPageFieldsAreBlank() {
+        final String name = "Flavia";
+        final Map<String, String> pageParams = Map.of("page", "", "size", "");
+        final String exceptionMessage = "Não é possivel realizar a operação, os parâmetros informados não são válidos.";
+
+        assertThatThrownBy(() -> underTest.apply(name, pageParams))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(exceptionMessage);
+    }
+
+    @Test
+    @DisplayName("When Find By Name Contains Persons With Page Fields Are Not Numerics Should Throw An IllegalArgumentException")
+    void whenPageFieldAreNotNumerics() {
+        final String name = "Flavia";
+        final Map<String, String> pageParams = Map.of("page", "zero", "size", "then");
+        final String exceptionMessage = "Não é possivel realizar a operação, os parâmetros `page` e `size` não " +
+                "são valores numéricos.";
+
+        Boolean expected = false;
+        Boolean actual = checkMapParamPageAreNumerics(pageParams);
+
+        assertEquals(expected, actual);
+
+        assertThatThrownBy(() -> underTest.apply(name, pageParams))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(exceptionMessage);
+    }
+
+    @Test
+    @DisplayName("When Find By Name Contains Persons With Page Fields Less Than Zero Should Throw An IllegalArgumentException")
+    void whenPageFieldAreLessThanZero() {
+        final String name = "Flavia";
+        final Map<String, String> pageParams = Map.of("page", "-10", "size", "-1");
+        final String exceptionMessage = "Não é possivel realizar a operação com os parâmetros informados sendo " +
+                "menores do que zero.";
+
+        assertThatThrownBy(() -> underTest.apply(name, pageParams))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(exceptionMessage);
+    }
+
+    private boolean checkMapParamPageAreNumerics(Map<String, String> pageParams) {
+        boolean isPageNumeric = checkFieldIsNumeric(pageParams.get("page"));
+        boolean isSizeNumeric = checkFieldIsNumeric(pageParams.get("size"));
+
+        return isPageNumeric && isSizeNumeric;
+    }
+
+    private boolean checkFieldIsNumeric(String field) {
+        try {
+            Integer.parseInt(field);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
     }
 
 }
