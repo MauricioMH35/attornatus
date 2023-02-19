052f4056317028577fa3daa9349d350cbb89454b
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-02-19 00:54:35 -0300

    feat: hateoas hyper media for person controller collections

---
+++ src/main/java/br/com/attornatus/resources/hateoas/processors/PersonCollectionProcessorHyperMedia.java
@@ -0,0 +1,28 @@
+package br.com.attornatus.resources.hateoas.processors;
+
+import br.com.attornatus.models.entities.Person;
+import br.com.attornatus.resources.hateoas.models.PersonModel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PersonCollectionProcessorHyperMedia {
+
+    private final DefaultPersonLinkHyperMedia linkHyperMedia;
+
+    public CollectionModel<PersonModel> process(CollectionModel<PersonModel> models) {
+        for (PersonModel model : models) {
+            linkHyperMedia.addLinkFindById(model);
+            linkHyperMedia.addLinkFindMainAddressByPersonId(model);
+        }
+
+        linkHyperMedia.addLinkSave(models);
+        linkHyperMedia.addLinkFindByBirth(models);
+        linkHyperMedia.addLinkFindByBirthBetween(models);
+
+        return models;
+    }
+
+}
