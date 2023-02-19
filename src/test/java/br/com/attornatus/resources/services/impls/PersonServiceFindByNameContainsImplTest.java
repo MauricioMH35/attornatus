e3fefd1bd82c59fe7eb02542f741542d60895d45
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-02-19 02:40:13 -0300

    feat: find by name contains when success return founded in service

--- src/test/java/br/com/attornatus/resources/services/impls/PersonServiceFindByNameContainsImplTest.java
+++ src/test/java/br/com/attornatus/resources/services/impls/PersonServiceFindByNameContainsImplTest.java
@@ -1,7 +1,9 @@
 package br.com.attornatus.resources.services.impls;
 
 import br.com.attornatus.exceptions.NotFoundException;
+import br.com.attornatus.models.entities.Person;
 import br.com.attornatus.models.repositories.PersonRepository;
+import br.com.attornatus.models.utils.PersonUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,7 +20,7 @@ import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 public class PersonServiceFindByNameContainsImplTest {
@@ -28,6 +30,27 @@ public class PersonServiceFindByNameContainsImplTest {
 
     @BeforeEach void setUp() {
         underTest = new PersonServiceFindByNameContainsImpl(repository);
+    }
+
+    @Test
+    @DisplayName("When Find By Name Contains Persons Should Return Page of Persons Founded")
+    void whenSuccessReturnPageOfPersons() {
+        final Map<String, String> pageParams = Map.of("page", "0", "size", "10");
+        final Integer page = Integer.parseInt(pageParams.get("page"));
+        final Integer size = Integer.parseInt(pageParams.get("size"));
+
+        assertEquals(0, page);
+        assertEquals(10, size);
+        final Pageable pageable = PageRequest.of(page, size, Sort.by("name").ascending());
+
+        final String name = "Flavia";
+        final Page<Person> founded = PersonUtil.newPersonsInPage();
+        when(repository.findByNameContains(name, pageable)).thenReturn(founded);
+
+        Page<Person> expected = PersonUtil.newPersonsInPage();
+        Page<Person> actual = underTest.apply(name, pageParams);
+        verify(repository, times(1)).findByNameContains(name, pageable);
+        assertEquals(expected, actual);
     }
 
     @Test
