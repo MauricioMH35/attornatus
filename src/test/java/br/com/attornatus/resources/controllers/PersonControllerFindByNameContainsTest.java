842f24152ecaa202f247661c8409a14dff77c64c
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-02-19 03:05:51 -0300

    feat: find by name contains when success find

--- src/test/java/br/com/attornatus/resources/controllers/PersonControllerFindByNameContainsTest.java
+++ src/test/java/br/com/attornatus/resources/controllers/PersonControllerFindByNameContainsTest.java
@@ -38,6 +38,26 @@ public class PersonControllerFindByNameContainsTest {
     }
 
     @Test
+    @DisplayName("When Find By Name Contains Persons Should Ok")
+    void whenSuccessReturnOk() throws Exception {
+        final String name = "Flavia";
+        final Map<String, String> pageParams = Map.of("page", "0", "size", "10");
+        final Page<Person> founded = PersonUtil.newSingleEntityInPage();
+        when(serviceFindByNameContains.apply(name, pageParams)).thenReturn(founded);
+
+        mvc.perform(get("/v1/api/people/name/"+name+"?page=0&size=10")
+                        .contentType(MediaType.parseMediaType("application/hal+json"))
+                        .accept(MediaType.parseMediaType("application/hal+json"))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.parseMediaType("application/hal+json")))
+                .andExpect(jsonPath("$._embedded.personModelList[0].content.id").value(1l))
+                .andExpect(jsonPath("$._embedded.personModelList[0].content.name").value("Flavia Matos"))
+                .andExpect(jsonPath("$._embedded.personModelList[0].content.birth").value("1990-08-23"));
+    }
+
+    @Test
     @DisplayName("When Find By Name Contains Persons Should Return Founded")
     void whenSuccessReturnPageOfPersons() throws Exception {
         final String name = "Flavia";
