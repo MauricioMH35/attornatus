565b0b204761d95f0e457fd9e00d0f84d1db695b
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-02-19 03:00:07 -0300

    feat: find by name contains page params test person controller

--- src/test/java/br/com/attornatus/resources/controllers/PersonControllerFindByNameContainsTest.java
+++ src/test/java/br/com/attornatus/resources/controllers/PersonControllerFindByNameContainsTest.java
@@ -55,4 +55,92 @@ public class PersonControllerFindByNameContainsTest {
                 .andExpect(jsonPath("$._embedded.personModelList[0].content.name").value("Flavia Matos"))
                 .andExpect(jsonPath("$._embedded.personModelList[0].content.birth").value("1990-08-23"));
     }
+
+    @Test
+    @DisplayName("When Find By Name Contains Persons With Page Fields Not Found Should Bad Request")
+    void whenPageFieldsAreNotFound() throws Exception {
+        final String name = "Flavia";
+        final Map<String, String> pageParams = Map.of("pageNoExist", "0", "sizeNoExist", "10");
+        final String pageKey = "pageNoExist";
+        final String pageValue = pageParams.get(pageKey);
+        final String sizeKey = "sizeNoExist";
+        final String sizeValue = pageParams.get(sizeKey);
+        final String exceptionMessage = "Não é possivel realizar a operação, os parâmetros informados não são válidos.";
+        when(serviceFindByNameContains.apply(name, pageParams)).thenThrow(new IllegalArgumentException(exceptionMessage));
+
+        mvc.perform(get("/v1/api/people/name/"+name+"?"+pageKey+"="+pageValue+"&"+sizeKey+"="+sizeValue)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect((jsonPath("$.message").value(exceptionMessage)));
+    }
+
+    @Test
+    @DisplayName("When Find By Name Contains Persons With Page Fields Are Blank Should Bad Request")
+    void whenPageFieldsAreBlank() throws Exception {
+        final String name = "Flavia";
+        final Map<String, String> pageParams = Map.of("page", "", "size", "");
+        final String pageKey = "page";
+        final String pageValue = pageParams.get(pageKey);
+        final String sizeKey = "size";
+        final String sizeValue = pageParams.get(sizeKey);
+        final String exceptionMessage = "Não é possivel realizar a operação, os parâmetros informados não são válidos.";
+        when(serviceFindByNameContains.apply(name, pageParams)).thenThrow(new IllegalArgumentException(exceptionMessage));
+
+        mvc.perform(get("/v1/api/people/name/"+name+"?"+pageKey+"="+pageValue+"&"+sizeKey+"="+sizeValue)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect((jsonPath("$.message").value(exceptionMessage)));
+    }
+
+    @Test
+    @DisplayName("When Find By Name Contains Persons With Page Fields Are Not Numerics Should Bad Request")
+    void whenPageFieldAreNotNumerics() throws Exception {
+        final String name = "Flavia";
+        final Map<String, String> pageParams = Map.of("page", "zero", "size", "ten");
+        final String pageKey = "page";
+        final String pageValue = pageParams.get(pageKey);
+        final String sizeKey = "size";
+        final String sizeValue = pageParams.get(sizeKey);
+        final String exceptionMessage = "Não é possivel realizar a operação, os parâmetros `page` e `size` não " +
+                "são valores numéricos.";
+        when(serviceFindByNameContains.apply(name, pageParams)).thenThrow(new IllegalArgumentException(exceptionMessage));
+
+        mvc.perform(get("/v1/api/people/name/"+name+"?"+pageKey+"="+pageValue+"&"+sizeKey+"="+sizeValue)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect((jsonPath("$.message").value(exceptionMessage)));
+    }
+
+    @Test
+    @DisplayName("When Find By Name Contains Persons With Page Fields Less Than Zero Should Bad Request")
+    void whenPageFieldAreLessThanZero() throws Exception {
+        final String name = "Flavia";
+        final Map<String, String> pageParams = Map.of("page", "-1", "size", "-10");
+        final String pageKey = "page";
+        final String pageValue = pageParams.get(pageKey);
+        final String sizeKey = "size";
+        final String sizeValue = pageParams.get(sizeKey);
+        final String exceptionMessage = "Não é possivel realizar a operação com os parâmetros informados sendo " +
+                "menores do que zero.";
+        when(serviceFindByNameContains.apply(name, pageParams)).thenThrow(new IllegalArgumentException(exceptionMessage));
+
+        mvc.perform(get("/v1/api/people/name/"+name+"?"+pageKey+"="+pageValue+"&"+sizeKey+"="+sizeValue)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect((jsonPath("$.message").value(exceptionMessage)));
+    }
+
+
 }
