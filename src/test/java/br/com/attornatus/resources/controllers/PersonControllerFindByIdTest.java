144939cfcb1b677dba3f9098236014fc5ce4ef67
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-02-19 02:12:51 -0300

    feat: optimize imports

--- src/test/java/br/com/attornatus/resources/controllers/PersonControllerFindByIdTest.java
+++ src/test/java/br/com/attornatus/resources/controllers/PersonControllerFindByIdTest.java
@@ -16,13 +16,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Optional;
-
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
