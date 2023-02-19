aa6845b06b9af04d5146a7acb1167b22bff873c6
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-02-19 01:04:20 -0300

    feat: beans zoneid and clock

--- src/main/java/br/com/attornatus/configs/ApplicationConfig.java
+++ src/main/java/br/com/attornatus/configs/ApplicationConfig.java
@@ -2,7 +2,20 @@ package br.com.attornatus.configs;
 
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Clock;
+import java.time.ZoneId;
+
 @Configuration
 public class ApplicationConfig {
+
+    @Bean
+    public ZoneId zoneId() {
+        return ZoneId.of("America/Sao_Paulo");
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.system(zoneId());
+    }
 
 }
