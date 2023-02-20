c6cf55f3011d8bd6ff7167f3eb3f2e3df73ea519
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-02-20 16:34:39 -0300

    feat: address controller

---
+++ src/main/java/br/com/attornatus/resources/controllers/AddressController.java
@@ -0,0 +1,65 @@
+package br.com.attornatus.resources.controllers;
+
+import br.com.attornatus.models.entities.Address;
+import br.com.attornatus.resources.hateoas.models.AddressModel;
+import br.com.attornatus.resources.hateoas.processors.AddressProcessor;
+import br.com.attornatus.resources.services.AddressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v1/api/addresses")
+@RequiredArgsConstructor
+public class AddressController {
+
+    private final AddressService service;
+    private final AddressProcessor processorHyperMedia;
+
+    @PostMapping("/{personId}")
+    public ResponseEntity<AddressModel> save(@PathVariable Long personId, @RequestBody Address address) {
+        Address saved = service.save(personId, address);
+        AddressModel model = processorHyperMedia.save(saved);
+        return ResponseEntity.ok(model);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressModel> findById(@PathVariable Long id) {
+        Address founded = service.findById(id);
+        AddressModel model = processorHyperMedia.findById(founded);
+        return ResponseEntity.ok(model);
+    }
+
+    @GetMapping("/person/{personId}")
+    public ResponseEntity<AddressModel> findMainAddressByPersonId(@PathVariable Long personId) {
+        Address founded = service.findMainAddressByPersonId(personId);
+        AddressModel model = processorHyperMedia.findMainAddressByPersonId(founded);
+        return ResponseEntity.ok(model);
+    }
+
+    @GetMapping("/persons/{personId}")
+    public ResponseEntity<CollectionModel<AddressModel>> findAllByPersonId(@PathVariable Long personId,
+                                                                           @RequestParam Map<String, String> pageParams) {
+        Page<Address> founded = service.findAllByPersonId(personId, pageParams);
+        CollectionModel<AddressModel> models = processorHyperMedia.findAllByPersonId(founded);
+        return ResponseEntity.ok(models);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AddressModel> updateById(@PathVariable Long id, @RequestBody Address address) {
+        Address updated = service.updateById(id, address);
+        AddressModel model = processorHyperMedia.udpateById(updated);
+        return ResponseEntity.ok(model);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteById(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
