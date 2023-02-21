784ee75dde27fc91bf52c63cd7565b31977306d5
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-02-21 00:45:36 -0300

    refact: change name main application

--- src/main/java/br/com/attornatus/PersonApplication.java
+++ src/main/java/br/com/attornatus/PersonApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
-public class PeopleApplication {
+public class PersonApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PeopleApplication.class, args);
+        SpringApplication.run(PersonApplication.class, args);
     }
 }
